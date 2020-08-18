1f258c1ff8e82d506c66a9caa21984c666a6d176
Kevin Olivera <kevin.oliviera@live.com>
2020-08-18 00:38:50 -0300

    save config files

--- destinations.sh
+++ destinations.sh
@@ -2,6 +2,6 @@
 
 declare -A dests
 dests=(
-    [i3wm/config]="../nice/i3/config"
-    [i3wm/i3status.conf]="../nice/i3/i3status.conf"
+    [i3wm/config]="../.config/i3/config"
+    [i3wm/i3status.conf]="../.config/i3/i3status.conf"
 )

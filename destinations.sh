3dd1f2f987e29fef248febd8c1aab9df8cb6e6bd
Kevin Olivera <kevin.oliviera@live.com>
2020-08-21 22:08:45 -0300

    rofi config

--- destinations.sh
+++ destinations.sh
@@ -4,4 +4,6 @@ declare -A dests
 dests=(
     [i3wm/config]="../.config/i3/config"
     [i3wm/i3status.conf]="../.config/i3/i3status.conf"
+    [rofi/config]="../.config/rofi/config"
+    [rofi/theme.rasi]="../.config/rofi/theme.rasi"
 )
